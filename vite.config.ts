fffe63dbe1961b3c0c24341af0346c00da6eb7dd
khanaena43-netizen <khanaena43@gmail.com>
2026-08-12 16:59:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/cbm--academy-website2/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
